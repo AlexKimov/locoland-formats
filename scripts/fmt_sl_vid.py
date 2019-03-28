24e2c6b771957ba8389a1b8db5615f9664693523
AlexKimov <alex.ea.li@yandex.ru>
2019-03-28 22:13:20 +0300

    add support for paletted sprites

--- scripts/fmt_sl_vid.py
+++ scripts/fmt_sl_vid.py
@@ -26,6 +26,7 @@ class LLSpriteArchive:
         self.type0 = 0
         self.spriteCount = 0
         self.paletteSize = 0
+        self.palette = []         
         self.sprites = []
         
     def parseHeader(self):
@@ -63,7 +64,7 @@ class LLSpriteArchive:
         imageData = filereader.readBytes(size)
               
         if type == 0: # image                   
-            formats = ["b4g4r4a4", "b5g6r5"] 
+            formats = ["b4g4r4a4", "r5g6b5"] 
             if self.spriteCount == 1: 
                 format = formats[1]
             else:
@@ -111,29 +112,88 @@ class LLSpriteArchive:
         #end = timer()       
         #print(end - start) 
         
-    def readDATASection(self, filereader):
-        size = filereader.readUInt()
-        filereader.seek(size, NOESEEK_REL)
-        #filereader.seek(12, NOESEEK_REL)
-        
-        #count = filereader.readUInt() 
-        
-        #for i in range(count):
-        #    filereader.seek(4, NOESEEK_REL) 
-        
-         #   dataSize = filereader.readUInt() 
-        #    imageData = filereader.readBytes(dataSize)
-        
-         #   if self.spriteCount > 1:
-         #       if self.paletteSize > 0:
-         #           self.sprites.append(self.palettedToRGBA32(imageData))
-            #else:
-                #self.spriteData += palettedToRGBA32(imageData)
+    def unpackSpriteData(self, filereader, width, height, dataSize): 
+        spriteData = bytearray(width*height*4)
     
+        if dataSize == 6:
+            filereader.seek(2, NOESEEK_REL)
+        else:           
+            # some data
+            dcount = filereader.readUShort()
+            filereader.seek(dcount*6, NOESEEK_REL) 
+            
+            yoffset = filereader.readUShort() # lines to skip from the beginning              
+            spriteLineNumber = filereader.readUShort() # number of the lines in sprite 
+            
+            for line in range(spriteLineNumber):
+                xoffset = filereader.readUByte() # offset to where image data begins
+                count = filereader.readUByte() # number of pixels to copy   
+                
+                # if xoffset = 0 - no more data in line or empty line  
+                
+                offsetInLine = 0 
+ 
+                while xoffset >= 0:
+                    if xoffset == 0 and count == 0:
+                        break               
+                
+                    offsetInLine += xoffset                
+                    pixelOffset = 0
+                      
+                    if self.type0 == 13 or self.type0 == 29:
+                        filereader.seek(count*2, NOESEEK_REL)  
+                        
+                    for pixelIndex in range(count):                                       
+                        # calculate position of image data to copy data
+                        # line - current line
+                        # yoffset - lines to skip from the beginning 
+                        # offsetInLine - start position in line
+                        # pixelIndex - current pixel pos to copy
+                        index = filereader.readUByte()                        
+                       
+                        pos = 4*((line + yoffset)*self.width + offsetInLine + \
+                            pixelIndex)
+                        
+                        spriteData[pos: pos + 4] = self.palette[index]
+                    
+                    offsetInLine += count 
+                  
+                    xoffset = filereader.readUByte()# if xoffset = 0 - end of line
+                    count = filereader.readUByte()
+                            
+        self.sprites.append(imageSprite(width, height, spriteData))
+        
+    def readDATASection(self, filereader):
+        filereader.seek(12, NOESEEK_REL)
+        
+        count = filereader.readUInt() 
+        for i in range(count):
+            dataSize = filereader.readUInt()        
+            
+            if self.type0 == 33:
+                width = filereader.readUShort()
+                height = filereader.readUShort()
+                                
+                self.unpackSpriteData(filereader, width, height, dataSize)
+            else:
+                filereader.seek(4, NOESEEK_REL)
+                self.unpackSpriteData(filereader, self.width, self.height, \
+                    dataSize)
+                         
     def readPALSection(self, filereader):    
         filereader.seek(16, NOESEEK_REL)
         self.paletteSize = filereader.readUInt()
-        self.palette = filereader.readBytes(self.paletteSize)       
+
+        alpha = (255).to_bytes(1, byteorder = "little") 
+        
+        for i in range(256):        
+            if (self.paletteSize == 768):                                
+                self.palette.append(filereader.readBytes(3) + alpha)
+            else:
+                self.palette.append(filereader.readBytes(4))            
+        
+        #noesis.logPopup()
+        #print(len(self.palette))
         
     def getData(self):
         while not self.reader.checkEOF():
@@ -145,7 +205,8 @@ class LLSpriteArchive:
             elif id == 541868368: # PAL
                 self.readPALSection(self.reader)
             else:
-                noesis.doException("Wrong format?")
+                #noesis.doException("Wrong format?")
+                break
  
  
 def locCheckType(data):
